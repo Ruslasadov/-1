12755df5ee9fecfe2917cfdd124b59684cde54b4
Ruslasadov <58813459+Ruslasadov@users.noreply.github.com>
2020-04-24 14:50:03 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+
+         <!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+   <h1></h1>
+    <?
+         echo "<h2>Day of the month, 2 digits with leading zero</h3>";
+         echo "<br/><br/>";
+         echo date(d);
+         echo "<br/><br/>";
+         echo "<h3>Full name of the day of the week</h1>";
+         echo "<br/><br/>";
+         echo date(l);
+         echo "<br/><br/>";
+         echo "<h4>Full name of the month</h1>";
+         echo "<br/><br/>";
+         echo date(F);
+         echo "<br/><br/>";
+         echo "<h5>Sequential month number with leading zero</h1>";
+         echo "<br/><br/>";
+         echo date(m);
+         echo "<br/><br/>";
+         echo "<h6>Sequence number of the year, 4 digits</h1>";
+         echo "<br/><br/>";
+         echo date(Y);
+         echo "<br/><br/>";
+         echo "<h2>mathematics</h1>";
+         echo "<br/>";
+         echo "<h4>6/3=";
+         $sum=6/3;
+         echo $sum;
+         echo "<h4>6-3=";
+         $sum=6-3;
+         echo $sum;
+         echo "<h4>6*3=";
+         $sum=6*3;
+         echo $sum;
+         echo "<h4>6+3=";
+         $sum=6+3;
+         echo $sum;
+
+    ?>
+</body>
+</html>
